f017770ce75b8fd6226b454c674dbb93b13c129a
Minh Lai Huynh Nhat <mlaihuynhnhat@openwt.com>
2023-07-26 11:43:53 +0700

    init app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+registerLocaleData(en);
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule, AppRoutingModule, BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: en_US }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/components/simple-modal/simple-modal.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from "@angular/common";
+import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { NzModalModule } from "ng-zorro-antd/modal";
+
+@Component({
+    selector: 'app-simple-modal',
+    standalone: true,
+    templateUrl: './simple-modal.component.html',
+    styleUrls: ['./simple-modal.component.scss'],
+    imports: [CommonModule, NzModalModule]
+})
+export class SimpleModalComponent {
+    @Input() public visible = false;
+    @Input() public title = '';
+    @Input() public modalContent = '';
+    @Output() public handleOk = new EventEmitter<void>();
+    @Output() public handleCancel = new EventEmitter<void>();
+
+    public onHandleOk(): void {
+        this.handleOk.emit();
+    }
+
+    public onHandleCancel(): void {
+        this.handleCancel.emit();
+    }
+}
