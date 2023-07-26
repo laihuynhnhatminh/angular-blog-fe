e681ec1d3d5742dc98912c84b2cbe9e022916b7e
Minh Lai Huynh Nhat <mlaihuynhnhat@gmail.com>
2023-07-26 20:02:19 +0700

    change component name

---
+++ src/app/shared/components/login/login.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-login',
+    standalone: true,
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+    imports: [CommonModule]
+})
+export class AppLoginComponent {
+
+}
