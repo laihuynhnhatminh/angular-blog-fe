6843d22a3c2e8f38869900be223940d763cb0dc9
Minh Lai Huynh Nhat <mlaihuynhnhat@openwt.com>
2023-07-31 15:36:09 +0700

    Small update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,18 @@ import en from '@angular/common/locales/en';
 import { NZ_I18N, en_US } from 'ng-zorro-antd/i18n';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { icons } from 'src/assets/icons/antd-icon';
 
 registerLocaleData(en);
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NzIconModule.forRoot(icons),
+  ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
 })
---
+++ src/assets/icons/antd-icon.ts
@@ -0,0 +1,8 @@
+import { IconDefinition } from '@ant-design/icons-angular';
+import {
+  LockFill,
+  LockOutline,
+  UserOutline,
+} from '@ant-design/icons-angular/icons';
+
+export const icons: IconDefinition[] = [LockFill, UserOutline, LockOutline];
